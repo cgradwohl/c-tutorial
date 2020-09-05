4f6bae975a45d27972161909de6bd13b90ae3c30
Chris Gradwohl <christophergradwohl@gmail.com>
2020-09-05 12:57:19 -0700

    Initial Commit

---
+++ c tutorial/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c tutorial
+//
+//  Created by Christopher Gradwohl on 9/5/20.
+//  Copyright © 2020 Christopher Gradwohl. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
